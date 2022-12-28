94d236389306746bcace31a1b7751c14117f35e8
archna2981 <71487798+archna2981@users.noreply.github.com>
2022-12-28 07:00:59 -0800

    Add files via upload

---
+++ file_system_events_traker.py
@@ -0,0 +1,52 @@
+#importing libraries
+import sys
+import time
+import os
+import random
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = r"C:\Users\Pragya\Downloads"
+
+#Event Hanlder Class
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created!")
+
+    def on_modified(self, event):
+        print(f"Hey, {event.src_path} has been modified!")
+
+    def on_moved(self, event):
+        print(f"Hey, {event.src_path} has been moved!")
+
+    def on_deleted(self, event):
+        print(f"Oops! Someone deleted {event.src_path}!")
+
+#Initialize Event Handler Class
+event_handler = FileEventHandler()
+
+#Initialize Observer
+observer = Observer()
+
+#Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+#Start the Observer
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("Running...")
+except KeyboardInterrupt:
+    print("Stopped!")
+    observer.stop()
+
+
+
+
+
+
+
